ef90258e1150669f8cb43087e2a6bfbf59f63eca
VishnuChebolu <cheboluvishnu@mail.com>
2022-05-13 19:09:19 +0530

    [+] init

---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+from utilities.logging.log import Log
+try:
+    import os
+    import sys
+    import argparse
+    import getpass
+    import configparser
+except:
+    Log.error("Missing modules detected!")
+    sys.exit(1)
+
+
+try:
+    from rich import print
+    from rich.table import Table
+except:
+    Log.error("rich module not found.")
+    sys.exit(1)
+
+
+try:
+    import puremagic as pr
+except:
+    Log.error("puremagic module not found.")
+    sys.exit(1)
+
+
+try:
+    import pyaxmlparser
+except:
+    Log.error("pyaxmlparser module not found.")
+    sys.exit(1)
+
+try:
+    from colorama import Fore, Style
+except:
+    Log.error("colorama module not found.")
+    sys.exit(1)
+
+pyaxmlparser.core.log.disabled = True
+
+red = Fore.LIGHTRED_EX
+cyan = Fore.LIGHTCYAN_EX
+white = Style.RESET_ALL
+green = Fore.LIGHTGREEN_EX
+
+
+infoC = f"{cyan}[{red}*{cyan}]{white}"
+infoS = f"[bold cyan][[bold red]*[bold cyan]][white]"
+foundS = f"[bold cyan][[bold red]+[bold cyan]][white]"
+errorS = f"[bold cyan][[bold red]![bold cyan]][white]"
+
+import utilities.modules.banners
---
+++ utilities/logging/log.py
@@ -0,0 +1,32 @@
+import sys
+
+class Log():
+    @staticmethod
+    def _print(word):
+        sys.stdout.write(word)
+        sys.stdout.flush()
+
+    @staticmethod
+    def info(word):
+        Log._print("\n\033[94m[~] {}\n\033[00m" .format(word))
+        Log._print('\n')
+
+    @staticmethod
+    def warning(word):
+        Log._print("\n\033[93m[!] {}\n\033[00m" .format(word))
+        Log._print('\n')
+
+    @staticmethod
+    def error(word):
+        Log._print("\n\033[91m[!] {}\n\033[00m" .format(word))
+        Log._print('\n')
+
+    @staticmethod
+    def success(word):
+        Log._print("\n\033[92m[+] {}\n\033[00m" .format(word))
+        Log._print('\n')
+
+    @staticmethod
+    def query(word):
+        Log._print("\n\033[93m[?] {}\n\033[00m" .format(word))
+        Log._print('\n')
