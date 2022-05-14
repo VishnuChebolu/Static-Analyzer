4b19a70672601e3623187f0d233ac027df2f3f5d
VishnuChebolu <cheboluvishnu@mail.com>
2022-05-14 07:04:51 +0530

    [+] metadata added

---
+++ utilities/modules/metadata.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+sys.path.append(os.getcwd()+"/utilities")
+from utilities.logging.log import Log
+
+try:
+    import exiftool
+except:
+    Log.error("PyExifTool module not found.")
+    sys.exit(1)
+
+try:
+    from rich import print
+except:
+    Log.error("rich module not found.")
+    sys.exit(1)
+
+
+infoS = f"[bold cyan][[bold red]*[bold cyan]][white]"
+
+def GetExif(mfile):
+    print(f"{infoS} Extracting metadata from target file...\n")
+
+    with exiftool.ExifTool() as et:
+        mdata = et.get_metadata(mfile)
+
+    print(f"{infoS} Exif/Metadata information")
+    for md in mdata:
+        try:
+            if "ExifTool" in md or "Error" in md:
+                pass
+            else:
+                print(f"[magenta]>>>[white] {md.split(':')[1]}: [green][i]{mdata[md]}[/i]")
+        except:
+            continue
+
+mfile = sys.argv[1]
+if os.path.isfile(mfile):
+    GetExif(mfile)
+else:
+    print("[blink bold white on red]Target file not found!")
